a0d095d9c596bb598331d5812688d4f620cf808f
john russelle domingo <johnrusselledomingo@gmail.com>
2023-05-12 10:54:13 +0800

    yoinkts

---
+++ src/test/java/example/cashcard/MyFirstTest.java
@@ -0,0 +1,12 @@
+package example.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MyFirstTest {
+    @Test
+    public  void myFirstTest(){
+        assertThat(42).isEqualTo(42);
+    }
+}
